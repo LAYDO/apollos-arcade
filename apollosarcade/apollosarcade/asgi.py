52831fcf7e3412e4e1e07773632620c615d7afbb
Landen Robinson <robinsonlanden@gmail.com>
2023-05-18 17:33:25 -0700

    1.5.3 - ASGI Update

--- apollosarcade/apollosarcade/asgi.py
+++ apollosarcade/apollosarcade/asgi.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apollosarcade.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from magic_fifteen import routing as magic_fifteen_routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apollosarcade.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
