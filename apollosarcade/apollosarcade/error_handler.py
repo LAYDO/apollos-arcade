0b2073963bb105a210fd7f9b4f35f59b7eb674eb
Landen Robinson <robinsonlanden@gmail.com>
2023-05-30 19:19:52 -0700

    2.0.4 - Cross Game Functionality Init
    - Beginnings of lobby, game, and post refactor

--- apollosarcade/apollosarcade/error_handler.py
+++ apollosarcade/apollosarcade/error_handler.py
@@ -1,3 +1,11 @@
 class LobbyError(Exception):
     def __init__(self, message):
         self.message = message
+
+class GameError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class PostError(Exception):
+    def __init__(self, message):
+        self.message = message
