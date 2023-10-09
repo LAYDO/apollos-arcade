46454756d44a6ca1f5e66ed598ce2dc56dfdcaf4
Landen Robinson <robinsonlanden@gmail.com>
2023-10-09 16:48:40 -0500

    Webpack Config Updates

--- apollosarcade/webpack.config.js
+++ apollosarcade/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, './static/dist/'),
+        path: path.resolve(__dirname, './static/'),
         clean: true,
     },
     plugins: [
