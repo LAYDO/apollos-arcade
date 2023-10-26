f781dccf71f27d25f1a307639ef49374844e849c
Landen Robinson <robinsonlanden@gmail.com>
2023-10-26 15:59:53 -0500

    Capture Logo & Feature Flag
    - Implemented feature flag for home to release Capture
    - Implemented Capture logos on arcade cabinet and game page
    - Hover implementation for arcade cabinets
    
    NEXT:
    - Start flushing out local Capture...
    - Implement backend to handle array of lines and provide to front-end
    - Draw board based on array provided by backend
    - Maybe push feature flag to backend
    
    FURTHER:
    - Implement local turn logic
    - Implement local square complete logic

--- apollosarcade/capture/views.py
+++ apollosarcade/capture/views.py
@@ -3,5 +3,4 @@ from django.http import JsonResponse
 
 # Create your views here.
 def capture(request):
-    return JsonResponse({'capture': 'Coming Soon!'})
-    # return render(request, 'capture_home.html',)
+    return render(request, 'capture_home.html',)
